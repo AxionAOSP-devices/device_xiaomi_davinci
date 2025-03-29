e4959505d136e3d34f88c6b3de734252a193bcb2
ryzenforce990 <superxorn@horizondroid.com>
2025-03-29 20:09:20 +0000

    davinci: axion 1/n
    Change-Id: If4d661fdcb5a223cb3608b16eadd740dc552bd88

--- lineage_davinci.mk
+++ lineage_davinci.mk
@@ -11,16 +11,22 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from davinci device
 $(call inherit-product, device/xiaomi/davinci/device.mk)
 
-# Inherit some common HorizonDroid stuff.
-$(call inherit-product, vendor/horizon/config/common_full_phone.mk)
+# Inherit some common Axion stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# HorizonDroid stuff.
-HORIZON_BUILD_TYPE := OFFICIAL
-HORIZON_MAINTAINER := superxorn
+# Axion Flags
+AXION_MAINTAINER := superxorn
+AXION_PROCESSOR := Snapdragon_730
+AXION_CPU_SMALL_CORES := 0,1,2,3
+AXION_CPU_BIG_CORES := 4,5,6,7
+AXION_CAMERA_REAR_INFO := 48
+AXION_CAMERA_FRONT_INFO := 20
+WITH_GMS := false
+
 
 TARGET_BOOT_ANIMATION_RES := 1080
 
-PRODUCT_NAME := horizon_davinci
+PRODUCT_NAME := lineage_davinci
 PRODUCT_DEVICE := davinci
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi 9T
